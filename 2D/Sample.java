33da054eed570a6c32935e24ebce0cfc54df289b
Kei <shindokate@gmail.com>
2023-09-09 18:29:57 +1000

    first commit

---
+++ 2D/Sample.java
@@ -0,0 +1,14 @@
+
+public class Sample {
+    public static void main(String[] args) {
+
+    }
+}
+
+public interface Item<T> {
+    // interface of
+    // player
+    // enemy
+    // score
+    void draw(T t);
+}
